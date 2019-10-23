2ff5a44176bc5c30e06fc2d1400d7a50485329a6
56592aya <yazdiha@ese.eur.nl>
2019-10-23 11:31:05 +0200

    can recover alpha

--- play.jl
+++ play.jl
@@ -17,4 +17,4 @@ theta_est,B1_est, B2_est, B1_truth, B2_truth,theta_truth, inds1, inds2 = do_Bs(m
 theta_truth_1,theta_truth_2,theta_est_1,theta_est_2 = do_plots(model, theta_est,B1_est, B2_est, B1_truth, B2_truth, theta_truth,inds1, inds2);
 @load "h_map" h_map
 (mean(model.γ[.!h_map])/25)[inds1, inds2]
- update_alpha_newton!(model, count_params, h_map)[inds1, inds2]
+update_alpha_newton!(model, count_params, h_map)[inds1, inds2]
